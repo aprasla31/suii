37e81374efd4cb84ac63eecea8b64ca5aa6c87bb
aprasla31 <108456886+aprasla31@users.noreply.github.com>
2022-10-11 23:16:12 +0530

    Update P2-iddfs.py

--- P2-iddfs.py
+++ P2-iddfs.py
@@ -1,13 +1,8 @@
-#501(prac2)----27/06/19----
-#AIM: Implement IDDFS(Iterative Deepening Depth-First Search).
-
 import queue as Q
 from RMP import dict_gn
-
 start='Arad'
 goal='Bucharest'
 result=''
-
 def DLS(city, visitedstack, startlimit, endlimit):
     global result
     found=0
@@ -22,7 +17,6 @@ def DLS(city, visitedstack, startlimit, endlimit):
             found=DLS(eachcity, visitedstack, startlimit+1, endlimit)
             if found:
                 return found
-
 def IDDFS(city, visitedstack, endlimit):
     global result
     for i in range(0, endlimit):
@@ -37,41 +31,10 @@ def IDDFS(city, visitedstack, endlimit):
             print("-----")
             result=' '
             visitedstack=[]
-
 def main():
     visitedstack=[]
     IDDFS(start, visitedstack, 9)
     print("IDDFS Traversal from ",start," to ", goal," is: ")
     print(result)
-
-
 main()            
 
-
-"""
-OUTPUT:
-Searching at Limit:  0
-Not Found! 
-Arad 
------
-Searching at Limit:  1
-Not Found! 
- Arad Zerind Sibiu Timisoara 
------
-Searching at Limit:  2
-Not Found! 
- Arad Zerind Oradea Sibiu Rimnicu Fagaras Timisoara Lugoj 
------
-Searching at Limit:  3
-Not Found! 
- Arad Zerind Oradea Sibiu Timisoara Lugoj Mehadia 
------
-Searching at Limit:  4
-Not Found! 
- Arad Zerind Oradea Sibiu Rimnicu Fagaras Timisoara Lugoj Mehadia Drobeta 
------
-Searching at Limit:  5
-Found
-IDDFS Traversal from  Arad  to  Bucharest  is: 
- Arad Zerind Oradea Sibiu Rimnicu Pitesti Craiova Fagaras Bucharest 
-"""
