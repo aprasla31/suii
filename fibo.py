d4fc9ea7116ac8dd5eb2bba9c1c8fa0827b885e8
aprasla31 <108456886+aprasla31@users.noreply.github.com>
2022-10-11 16:59:10 +0530

    Delete fibo.py

--- fibo.py
+++
@@ -1,9 +0,0 @@
-def Fibonacci(n):
-    if (n<=1):
-        return n
-    else:
-        return ((Fibonacci(n-1))+(Fibonacci(n-2)))
-n=int(input("Enter no of terms: "))
-print("Fibonacci Seq: ")
-for i in range(n):
-    print(Fibonacci(i))
